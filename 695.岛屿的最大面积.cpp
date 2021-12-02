365399636f55b1fd2c140a9205c825446951920b
chenshuquan <chenshuquan@kuaishou.com>
2021-12-02 15:56:46 +0800

    max island

---
+++ 695.岛屿的最大面积.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=695 lang=cpp
+ *
+ * [695] 岛屿的最大面积
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea = 0;
+        for(int i = 0; i < grid.size(); ++i) {
+            for(int j = 0; j < grid[0].size(); ++j) {
+                int area = 0;
+                std::stack<int> nodeStack;
+                nodeStack.push(j);
+                nodeStack.push(i);
+                int xs[4] = {1, -1, 0, 0};
+                int ys[4] = {0, 0, 1, -1};
+                while(!nodeStack.empty()) {
+                    int x = nodeStack.top();
+                    nodeStack.pop();
+                    int y = nodeStack.top();
+                    nodeStack.pop();
+                    if(x<0||y<0||x>=grid.size()||y>=grid[0].size()||grid[i][j]==0)
+                        continue;
+                    ++area;
+                    grid[i][j] = 0;
+                    for(int k = 0; k<4; ++k) {
+                        nodeStack.push(y + ys[k]);
+                        nodeStack.push(x + xs[k]);
+                    }
+                }
+                maxArea = std::max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+};
+// @lc code=end
+
