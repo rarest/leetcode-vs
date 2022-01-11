e737f61f1ffc6069fdb55018e5d71b3b1b89cadd
rarest <>
2022-01-11 19:32:08 +0800

    frequency k

--- 347.前-k-个高频元素.cpp
+++ 347.前-k-个高频元素.cpp
@@ -8,10 +8,30 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
+    std::unordered_map<int, int> buckets;
+    int maxCount = 0;
+    for (const int &val: nums) {
+        maxCount = std::max(maxCount, ++buckets[val]);
+    }
+    vector<vector<int> > frequentArray(buckets.size() + 1);
+    for (const auto &bucket: buckets) {
+        frequentArray[bucket.second].push_back(bucket.first);
+    }
+    int j = 0;
+    std::vector<int> ret;
+    for (size_t i = frequentArray.size() - 1; i >= 0 && j < k; i--) {
+        for (const int &val: frequentArray[i]) {
+            if (j < k) {
+                ret.push_back(val);
+                ++j;
+            } else
+                break;
+        }
+    }
+    return ret;
     }
 
-    
+
 };
 // @lc code=end
 
